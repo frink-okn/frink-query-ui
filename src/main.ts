69b81814897e41190226831bc2cbbb9042a39db9
David Glymph <Woozl@users.noreply.github.com>
2024-06-21 16:44:33 -0400

    add Inter font and preload

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import LaraTheme from "@primevue/themes/lara"
+import Tooltip from 'primevue/tooltip';
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: LaraTheme
+  }
+});
 
-app.mount('#app')
+app.directive('tooltip', Tooltip);
+
+app.mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
+import Button from "primevue/button"
 </script>
 
 <template>
+  <h1>Home page</h1>
+  <h2>Home page</h2>
+  <h3>Home page</h3>
+  <h4>Home page</h4>
+  <h5>Home page</h5>
+  <h6>Home page</h6>
+  <p>Home page</p>
+  <Button v-tooltip="'Tooltip'">
+    Hello
+  </Button>
 </template>
 
 <style scoped>
