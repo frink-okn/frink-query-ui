634ab76e808d79bd93bd0dd34df7104032227ebf
David Glymph <Woozl@users.noreply.github.com>
2024-06-28 11:59:57 -0400

    add attribution text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,7 @@ import Results from "@/components/Results.vue";
 
 <template>
   <div class="padding-wrapper">
-    <Panels>
+    <Panels class="panels">
       <template v-slot:examples>
         <Examples />
       </template>
@@ -26,6 +26,19 @@ import Results from "@/components/Results.vue";
         <Results />
       </template>
     </Panels>
+    <footer>
+      <small>
+        <a href="https://renci.org/" target="_blank" rel="noopener noreferrer">RENCI</a>
+      </small>
+      <small>
+        <a href="https://github.com/frink-okn/frink-query-ui/" target="_blank" rel="noopener noreferrer">
+          Source code available on <span class="pi pi-github"></span>
+        </a>
+      </small>
+      <small>
+        Powered by <a href="https://comunica.dev/" target="_blank" rel="noopener noreferrer">Comunica</a>
+      </small>
+    </footer>
   </div>
 </template>
 
@@ -33,11 +46,31 @@ import Results from "@/components/Results.vue";
 .padding-wrapper {
   height: 100%;
   padding: 1rem;
+  padding-bottom: 0;
+
+  display: flex;
+  flex-direction: column;
+}
+
+.panels {
+  flex: 1;
+}
+
+footer {
+  padding: 8px;
+  text-align: center;
+}
+
+footer > small:not(:last-of-type)::after {
+  content: "|";
+  color: var(--p-slate-500);
+  margin: 0px 1ch 0px 1.5ch;
 }
 
 @media (max-width: 800px) {
   .padding-wrapper {
     padding: 0.5rem;
+    padding-bottom: 0;
   }
 }
 </style>
