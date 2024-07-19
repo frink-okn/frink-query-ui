e0a0b0eab1a7269652007a0cfadba78d06223da4
David Glymph <Woozl@users.noreply.github.com>
2024-07-19 12:30:56 -0400

    add a subtle blur to hide line background behind attribution text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -28,22 +28,25 @@ import Results from '@/components/Results.vue'
     </Panels>
 
     <footer>
-      <small>
-        <a href="https://renci.org/" target="_blank" rel="noopener noreferrer">RENCI</a>
-      </small>
-      <small>
-        <a
-          href="https://github.com/frink-okn/frink-query-ui/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Source code available on <span class="pi pi-github"></span>
-        </a>
-      </small>
-      <small>
-        Powered by
-        <a href="https://comunica.dev/" target="_blank" rel="noopener noreferrer">Comunica</a>
-      </small>
+      <div class="attribution-text">
+        <div class="background-fader" aria-hidden="true"></div>
+        <small>
+          <a href="https://renci.org/" target="_blank" rel="noopener noreferrer">RENCI</a>
+        </small>
+        <small>
+          <a
+            href="https://github.com/frink-okn/frink-query-ui/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Source code available on <span class="pi pi-github"></span>
+          </a>
+        </small>
+        <small>
+          Powered by
+          <a href="https://comunica.dev/" target="_blank" rel="noopener noreferrer">Comunica</a>
+        </small>
+      </div>
     </footer>
   </div>
 </template>
@@ -64,14 +67,32 @@ import Results from '@/components/Results.vue'
 }
 
 footer {
-  padding: 8px;
-  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-footer > small:not(:last-of-type)::after {
+.attribution-text {
+  padding: 8px;
+  text-align: center;
+  position: relative;
+}
+
+.attribution-text > small:not(:last-of-type)::after {
   content: '|';
   color: var(--p-slate-500);
   margin: 0px 1ch 0px 1.5ch;
+}
+
+.background-fader {
+  background-color: var(--p-slate-50);
+  filter: blur(10px);
+  z-index: -1;
+  position: absolute;
+  top: 8px;
+  bottom: 8px;
+  left: 0px;
+  right: 0px;
 }
 
 @media (max-width: 800px) {
