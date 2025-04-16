b078a1c121b488dff7928aeb909afddf90bfd99a
Danny O'Neal <danny@scales-okn.org>
2025-04-15 17:21:02 -0700

    Update SCALES KG example queries

--- src/data/examples.ts
+++ src/data/examples.ts
@@ -116,11 +116,16 @@ WHERE {
   {
     title: "List SCALES ontology event labels",
     sources: ["scales"],
-    query: `PREFIX scales: <http://schemas.scales-okn.org/rdf/scales#>
-SELECT DISTINCT ?ontologyLabel
+    query: `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX j: <http://release.niem.gov/niem/domains/jxdm/7.2/#>
+PREFIX nc: <http://release.niem.gov/niem/niem-core/5.0/>
+
+SELECT DISTINCT ?judgeName
 WHERE {
-    ?s scales:hasOntologyLabel ?ontologyLabel .
-}`,
+    ?judge rdf:type j:Judge ;
+            nc:PersonFullName ?judgeName .
+}
+`,
   },
   {
     title:
