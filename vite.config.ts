70b81fd71643f105c02aad06d02d4b681ec09b80
David Glymph <Woozl@users.noreply.github.com>
2025-05-13 10:12:38 -0400

    make router use basepath, if available.
    this requires allowing this environment variable in the client bundle. To use the client env variables in the vite config we use code from this stack overflow answer to combine them into the process.env object:
    
    https://stackoverflow.com/questions/66389043/how-can-i-use-vite-env-variables-in-vite-config-js

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
@@ -7,21 +7,25 @@ const ReactCompilerConfig = {
 }
 
 // https://vite.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@mui/material': '@mui/joy'
-    }
-  },
-  plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    react({
-      babel: {
-        plugins: [
-          ["babel-plugin-react-compiler", ReactCompilerConfig],
-        ]
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') }
+  
+  return {
+    resolve: {
+      alias: {
+        '@mui/material': '@mui/joy'
       }
-    }),
-  ],
-  base: process.env.BASE_URL,
+    },
+    plugins: [
+      TanStackRouterVite({ autoCodeSplitting: true }),
+      react({
+        babel: {
+          plugins: [
+            ["babel-plugin-react-compiler", ReactCompilerConfig],
+          ]
+        }
+      }),
+    ],
+    base: process.env.VITE_BASE_URL,
+  }
 })
