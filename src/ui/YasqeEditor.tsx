048acb0de7c5e59237e415f6a8d18de685d2d4f7
David Glymph <Woozl@users.noreply.github.com>
2025-08-26 18:36:54 -0400

    remove lint suppression by fixing YasqeEditor dependency array

--- src/ui/YasqeEditor.tsx
+++ src/ui/YasqeEditor.tsx
@@ -18,15 +18,12 @@ export function YasqeEditor({ value, onChange }: YasqeEditorProps) {
     }
 
     yasqeInstance.current = new Yasqe(divEl.current);
-    yasqeInstance.current.setValue(value);
 
     return () => {
       yasqeInstance.current?.destroy();
       yasqeInstance.current = null;
     };
-    // eslint-disable-next-line react-compiler/react-compiler
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); // `value` is handled by the following useEffect
+  }, []);
 
   useEffect(() => {
     if (yasqeInstance.current && yasqeInstance.current.getValue() !== value) {
